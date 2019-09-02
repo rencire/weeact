0151a75dcc322c25ed374a0c353abf10c2724c08
Eric Ren <546296+rencire@users.noreply.github.com>
2019-09-01 20:12:54 -0700

    updated lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "*.{ts,tsx}": [
     "prettier --write",
-    "tslint -c tslint.json 'src/**/*.ts' --fix",
+    "tslint -c tslint.json --fix",
     "git add",
     "jest --bail --findRelatedTests"
   ],
